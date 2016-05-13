ee3b774fb3a59943c8cf8b0f7a4f68e4e6fcf9cd
Maximilian Algehed <m.algehed@gmail.com>
2016-05-13 15:46:27 +0200

    Quaternionsgit add Rollercoasters.hs git add Rollercoasters.hs

--- src/Rollercoasters/Rollercoasters.hs
+++ src/Rollercoasters/Rollercoasters.hs
@@ -1,60 +1,62 @@
+import Data.Vect.Float.Base
+import Data.Vect.Float.Util.Quaternion
 import Prelude hiding (repeat)
 
-type Vector3       = (Double, Double, Double)
+type Coordinate    = Vec3
 
-type Coordinate    = Vector3
+type Radians       = Float
 
-type Viewvector    = Vector3
+type Time          = Float 
 
-type Time          = Double
-
-data Result        = Going (Coordinate, Viewvector) | Done Time (Coordinate, Viewvector) deriving (Show)
+data Result        = Going (Coordinate, UnitQuaternion) | Done Time (Coordinate, UnitQuaternion) deriving (Show)
 
 type Rollercoaster = (Time -> Result) -> (Time -> Result)
-
-muld :: (Coordinate, Viewvector) -> Coordinate -> Time -> (Coordinate, Viewvector)
-muld ((a0, a1, a2), v) (c0, c1, c2) t = ((a0+c0*t, a1+c1*t, a2+c2*t), v)
 
 -- Sequential composition of rollercoasters
 (<>) :: Rollercoaster -> Rollercoaster -> Rollercoaster
 (<>) r r' f t = case r f t of
-                v@(Going _)    -> v
-                Done t' cv     -> ((delay (t - t') r') . r) f t
+                    v@(Going _)    -> v
+                    Done t' cv     -> ((delay (t - t') r') . r) f t
 
-start :: Time -> Result
-start t = Done t ((0, 0, 0), (0, 0, 0))
-
-direction :: Vector3 -> Time -> Rollercoaster
-direction d dur f t
-    | t < dur   = case f t of
-                    Going cv   -> Going (muld cv d t) 
-                    Done t' cv -> Going (muld cv d t)
-    | otherwise = case f t of
-                    Going cv   -> Going (muld cv d dur)
-                    Done t' cv -> Done (min t' (t-dur)) (muld cv d dur) 
-
+-- Delay a rollercoaster
 delay :: Time -> Rollercoaster -> Rollercoaster
 delay t r f t' = r (\_ -> f t') (max (t' - t) 0)
 
+-- Keep repeating a rollercoaster forever
 forever :: Rollercoaster -> Rollercoaster
 forever r = r <> forever r
 
-loop :: Time -> Double -> Rollercoaster 
-loop dur r f t
-    | t < dur   = case f t of
-                    Going cv    -> Going (muld cv (r*(sin (2*pi*t/dur)), 0, r-r*(cos (2*pi*t/dur))) 1)
-                    Done  t' cv -> Going (muld cv (r*(sin (2*pi*t/dur)), 0, r-r*(cos (2*pi*t/dur))) 1)
-    | otherwise = case f t of
-                    Going cv   -> Going cv
-                    Done t' cv -> Done (min t' (t - dur)) cv
-
+-- Repeat a rollercoaster a limited number of times
 repeat :: Int -> Rollercoaster -> Rollercoaster
 repeat 0 _ = id
 repeat n r = r <> repeat (n-1) r
 
+-- Limit the running time of the rollercoaster
 limit :: Time -> Rollercoaster -> Rollercoaster
 limit d r f t
     | t <= d    = r f t
     | otherwise = case r f d of
                     Going cv   -> Done (t - d) cv 
                     Done t' cv -> Done (t' + t - d) cv
+
+-- The start of a rollercoaster
+start :: Time -> Result
+start t = Done t (Vec3 0.0 0.0 0.0, unitU)
+
+-- Rotate a rollercoaster
+rotate :: Vec3 -> Radians -> Rollercoaster -> Rollercoaster 
+rotate v rad r f = r rot  
+    where
+        rot t = case f t of
+                    Going (v', q)   -> Going (v', multU q (rotU v rad))
+                    Done t' (v', q) -> Done t' (v', multU q (rotU v rad))
+
+-- Go straight with a given velocity for a given amount of time
+direction :: Vec3 -> Time -> Rollercoaster
+direction d dur r t
+    | t <= dur  = case r t of
+                    Going (v, q)   -> Going (v &+ (t *& (actU q d)), q) 
+                    Done t' (v, q) -> Going (v &+ (t *& (actU q d)), q)
+    | otherwise = case r t of
+                    Going (v, q)   -> Going (v &+ (dur *& (actU q d)), q)
+                    Done t' (v, q) -> Done (min t' (t - dur)) (v &+ (dur *& (actU q d)), q)
